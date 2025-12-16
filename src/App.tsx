0a30c8026468c866afe516bd7edeaae369f26a70
evanEDTX <evan@endiatx.com>
2025-12-16 11:48:00 -0800

    stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import {ReactNode, useState } from "react"
-import HomePage from "./Pages/Home"
 
 export var PageSetter = (_: ReactNode) => {} 
 
 function App() {
-    const [currentPage, setCurrentPage] = useState<ReactNode>(<HomePage/>)
 
-    PageSetter = setCurrentPage
 
-    return currentPage
+    return (
+        <div>
+
+        </div>
+    )
 }
 
 export default App
